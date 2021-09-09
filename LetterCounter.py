eb64e10556e1e6c04618d2d16b46d1692b59c332
Vik <jzenterprize90@gmail.com>
2021-09-09 17:51:04 -0400

    Changed format and added comments

--- LetterCounter.py
+++ LetterCounter.py
@@ -1,6 +1,14 @@
 user_msg = input("Please enter your message ").lower()
 user_letter = input("Please enter your letter ").lower()
 
+# Count occurrences
+count = user_msg.count(user_letter)
 
+# Calculate percentage
+len_message = len(user_msg)
+percentage = count/len_message * 100
+
+# Print output to user
 print(f"The letter '{user_letter}' occurs {user_msg.count(user_letter)}")
+print(percentage)
 
