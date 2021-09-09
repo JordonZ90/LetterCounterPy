dc217dc102830a5c312eef71c3b0ea187aade0f7
Vik <jzenterprize90@gmail.com>
2021-09-09 17:08:38 -0400

    Simple Py LEtter counter

---
+++ LetterCounter.py
@@ -0,0 +1,6 @@
+user_msg = input("Please enter your message ").lower()
+user_letter = input("Please enter your letter ").lower()
+
+# print(user_msg.count(user_letter))
+print(f"The letter '{user_letter}' occurs {user_msg.count(user_letter)}")
+
